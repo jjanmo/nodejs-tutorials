1c93f39ca63cce4bd7236643bf2f70797683cb0c
jjanmo <jjanmo07@hanmail.net>
2019-12-24 20:15:31 +0900

    Add write-file  and store-file of submitted data

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const express = require('express'); //import express
+const express = require('express'); //import module express
+const fs = require('fs');           //import module file system
 const app = express();              //use express
 const port = 3000;
 app.listen(port, function () {
@@ -8,6 +9,7 @@ app.listen(port, function () {
 //template engine을 사용하기 위한 코드
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.locals.pretty = true; //pug파일에서 만든 html이 웹상에서 예쁘게 보이도록 하는 설정
 
 //post방식의 정보를 받기위한 코드(body를 이용하기위한)
 app.use(express.json());
@@ -18,7 +20,14 @@ app.get('/topic/new', function (req, res) {
 });
 
 app.post('/form', function (req, res) {
+    //form을 통해서 받은 title과 description을 파일에 저장
     const title = req.body.title;
     const description = req.body.description;
-    res.send(title + ',' + description);
+    fs.writeFile(`./data/${title}.txt`, description, function (err) {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+        }
+        console.log('The file has been saved!');
+    });
+    res.send('Success');
 });
