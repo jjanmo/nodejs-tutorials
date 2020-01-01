6024909bc94f10192c8dd0a1e755395882f17b04
jjanmo <jjanmo07@hanmail.net>
2020-01-02 02:04:30 +0900

    Add count router using session

--- project1_o2/app_session.js
+++ project1_o2/app_session.js
@@ -6,9 +6,17 @@ app.listen(port, function () {
     console.log(`App listening on port ${port}`);
 });
 
+//session을 사용할 준비 완료!!
 app.use(session({
     secret: 'asdfasdfasf@#$%safdsfa',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true }
-}))
+}));
+
+
+app.get('/count', function (req, res) {
+    console.log(req.session.count);
+    req.session.count = req.session.count ? req.session.count + 1 : 1;
+    res.send(`count : ${req.session.count}`);
+});
+//session으로 만들어졌기때문에 서버가 리로드될 때마다 리셋됨
