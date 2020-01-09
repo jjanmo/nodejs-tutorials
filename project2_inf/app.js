c9b8761b4ef0dfc833ccc68f584f177afc296461
jjanmo <jjanmo07@hanmail.net>
2020-01-10 01:08:07 +0900

    Add template engine 'ejs' and add ajax in sendAjax.html
    part related to ajax  is not completed yet

--- project2_inf/app.js
+++ project2_inf/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs'); //embedded javascript
 const app = express();
 const port = 3000;
 app.listen(port, () => console.log(`start!!! listen on the port ${port}`));
@@ -14,7 +15,12 @@ app.use(express.static('public'));
 //bodyparser 등록
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+//이제 express에 bodyparser일부 기능이 빌트인되었음, bodyparser를 임포트하지않아도 사용가능
+//-> app.use(express.json()); 
+//-> app.use(express.urlencoded({extened:false}));
 
+//ejs 등록
+app.set('view engine', 'ejs');
 
 //url routing 
 app.get(['/', '/main'], function (req, res) {
@@ -27,5 +33,23 @@ app.post('/sendUserInfo', function (req, res) {
     const userName = req.body.userName;
     const userEmail = req.body.userEmail;
     const userPhone = req.body.userPhone;
-    res.send(`${userName}, ${userEmail}, ${userPhone}`);
+    // res.send(`${userName}, ${userEmail}, ${userPhone}`);
+    res.render('userinfo', {
+        userName: userName,
+        userEmail: userEmail,
+        userPhone: userPhone
+    });
+    //전달받은 변수값과 HTML을 합쳐서 클라이언트쪽에 보여주기 위해서 ejs를 이용함
+});
+
+//ajax 이용
+app.get('/sendAjax', function (req, res) {
+    res.sendFile(__dirname + '/public/sendAjax.html');
+});
+
+app.post('/sendAjax', function (req, res) {
+    console.log(req.body.userInfo);
+    const responseData = { data: req.body.userInfo };
+    console.log(responseData);
+    res.json(responseData);
 });
