a2be4b2da72bc974822efc3f10c3d495ccf4eafb
jjanmo <jjanmo07@hanmail.net>
2020-01-23 13:53:39 +0900

    Add storing session data in orientDB at app_session_orient.js

--- project1_o2/app_session_orient.js
+++ project1_o2/app_session_orient.js
@@ -2,9 +2,9 @@
 
 const express = require("express");
 const session = require("express-session");
-//express에는 session기능이 없음 -> express에서 session에 대한 구체적인 기능을 하는 것이 express-session
-const MySQLStore = require('express-mysql-session')(session);
+//-> express에는 session기능이 없음 -> express에서 session에 대한 구체적인 기능을 하는 것이 express-session
 
+const OrientoStore = require('connect-oriento')(session);
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
@@ -12,26 +12,26 @@ app.listen(port, function () {
     console.log(`App listening on port ${port}`);
 });
 
-//session을 어디에 저장할지에 대한 정보 등록 : mysql
-//-> 만약에 mysql을 정보저장을 위해서 사용하고 또한 session저장을 위해서 사용한다면 2가지 모두 등록해야함
-const sessionStore = new MySQLStore({
-    //options 정보
-    host: 'localhost',
-    port: 3306,
-    user: 'jjanmo',
-    password: 'jjanmo',
-    database: 'o2'
-});
+/*
+const config = {
+    session: {
+        server: "host=localhost&port=2424&username=root&password=jjanmo&db=o2"
+    }
+};
+ ~~ : new OrientStore(config.session);
+*/
 
 app.use(session({
     key: 'session_cookie_name',
     secret: 'session_cookie_secret',
-    store: sessionStore,
+    store: new OrientoStore({
+        server: "host=localhost&port=2424&username=root&password=jjanmo&db=o2"
+    }),
     resave: false,
     saveUninitialized: false
 }));
 
-//-> 이렇게 설정하면 database 'o2'에 sessions라는 테이블이 생기고 그곳에 session정보가 저장됨
+//-> 이렇게 설정하면 database 'o2'에 sessions라는 스키마가 생기고 그곳에 session정보가 저장됨
 
 /*
 참고:설명추가
@@ -92,7 +92,7 @@ app.get("/auth/logout", function (req, res) {
     // res.redirect("/welcome");
 
     //좀 더 안전한 redirection을 위한 작업)(아래) : database에 session을 저장하고 난 후에 콜백함수를 실행
-    //-> database에 반영이 안된채로 redirect되는 경우를 막을 수 있음
+    //-> database에 반영이 안된 채로 redirect되는 경우를 막을 수 있음
     //-> redirect하는 경우에만 사용하면 됨
     req.session.save(function () {
         res.redirect("/welcome");
