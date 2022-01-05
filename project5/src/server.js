c431c32a17022068aa6926cc74f4825e0c4ab005
jjanmo <jjanmo07@hanmail.net>
2022-01-06 00:26:26 +0900

    feat: add front views using pug template

--- project5/src/server.js
+++ project5/src/server.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
-const port = 3000;
+const port = 8000;
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
