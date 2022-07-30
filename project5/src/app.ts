5c6ca887be317f6c2c4fb66e6fcf5c80bf8368ce
jjanmo <jjanmo07@hanmail.net>
2022-07-30 14:40:38 +0900

    fix: configure starting server with typescript

--- project5/src/app.ts
+++ project5/src/app.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express'
 
 const app = express()
-const port = 5000
+const port = 8080
 
 app.get('/', (req: Request, res: Response) => {
   res.send('hello world')
@@ -10,5 +10,3 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
-
-console.log('hwllo world')
