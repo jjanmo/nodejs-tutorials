13c9d23544128726d6e5734cfd89561b75cf845e
jjanmo <jjanmo07@hanmail.net>
2023-11-18 17:16:29 +0900

    fix: 빌드시 views폴더를 못찾는 이슈 수정

--- project3/src/server.ts
+++ project3/src/server.ts
@@ -3,4 +3,11 @@ import express from 'express'
 const PORT = 4000
 const app = express()
 
+app.set('view engine', 'pug')
+app.set('views', process.cwd() + '/src/views')
+app.use('/public', express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => res.render('home'))
+app.get('/*', (req, res) => res.redirect('/'))
+
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
