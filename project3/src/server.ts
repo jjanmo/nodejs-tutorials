5a01cd10b315e2b6dfcdd197f24d248c736a0445
jjanmo <jjanmo07@hanmail.net>
2024-01-10 23:13:22 +0900

    feat: configure static path

--- project3/src/server.ts
+++ project3/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express'
+import path from 'path'
 
 const PORT = 4000
 const app = express()
 
 app.set('view engine', 'pug')
 app.set('views', process.cwd() + '/src/views')
-app.use('/public', express.static(__dirname + '/public'))
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => res.render('home'))
 app.get('/*', (req, res) => res.redirect('/'))
