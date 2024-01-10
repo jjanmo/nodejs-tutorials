5cb905a662a42aece6f68bcdb9adb75c741c8a9b
jjanmo <jjanmo07@hanmail.net>
2024-01-10 23:52:11 +0900

    feat: connect websocket with http-server

--- project3/src/server.ts
+++ project3/src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import path from 'path'
+import http from 'http'
+import WebSocket from 'ws'
 
 const PORT = 4000
 const app = express()
@@ -12,4 +14,9 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => res.render('home'))
 app.get('/*', (req, res) => res.redirect('/'))
 
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+const handleListen = () => console.log(`Listening on http://localhost:${PORT}`)
+
+const server = http.createServer(app)
+new WebSocket.Server({ server })
+
+server.listen(PORT, handleListen)
