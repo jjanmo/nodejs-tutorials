4ed9af3941d1105eb4be2d2032eebf0713fe4aa4
jjanmo <jjanmo07@hanmail.net>
2024-04-19 01:11:32 +0900

    chores: 더미데이터 추가

--- project3/src/controllers/index.ts
+++ project3/src/controllers/index.ts
@@ -1,9 +1,28 @@
 import { Request, Response } from 'express'
 
+const dummyMessages = [
+  {
+    nickname: 'jjanmo',
+    body: '안녕하세요',
+  },
+  {
+    nickname: 'leonard',
+    body: '하욤!!!',
+  },
+  {
+    nickname: 'jjanmo',
+    body: '잘지내니? 오랜만이쿤!!',
+  },
+  {
+    nickname: 'leonard',
+    body: '너도 오랜만이구나!!',
+  },
+]
+
 export const home = (req: Request, res: Response) => {
   res.render('home', { title: 'Home' })
 }
 
 export const chat = (req: Request, res: Response) => {
-  res.render('chat', { title: 'Chat' })
+  res.render('chat', { title: 'Chat', messages: dummyMessages })
 }
