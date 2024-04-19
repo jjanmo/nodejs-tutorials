d4db992288b6c4bd7fad073ca9df1bafca1b83bc
jjanmo <jjanmo07@hanmail.net>
2024-04-20 01:43:16 +0900

    feat: 메시지 랜더링 로직 추가

--- project3/src/public/js/chat.js
+++ project3/src/public/js/chat.js
@@ -1,30 +1,87 @@
-const $form = document.querySelector('form')
-const $input = document.querySelector('input')
-const $button = document.querySelector('button')
-// const $list = document.querySelector('ul')
+const chatForm = document.querySelector('.chat-form')
+const input = chatForm.querySelector('input')
+const button = chatForm.querySelector('button')
 
 const socket = new WebSocket(`ws://${window.location.host}`)
 
 socket.addEventListener('open', () => {
   console.log('Connected to Server 🚀')
-})
 
-socket.addEventListener('message', (message) => {
-  console.log('New Message: ', message.data)
+  const nickname = getNickname()
+  const _message = {
+    type: 'connection',
+    data: nickname,
+  }
+  socket.send(JSON.stringify(_message))
 })
 
 socket.addEventListener('close', () => {
   console.log('Disconnected from Server ✋🏻')
+
+  const nickname = getNickname()
+  const _message = {
+    type: 'close',
+    data: nickname,
+  }
+  socket.send(JSON.stringify(_message))
+})
+
+socket.addEventListener('message', (message) => {
+  const { data, type } = JSON.parse(message.data)
+
+  switch (type) {
+    case 'connection':
+      renderConnectedNumber(data)
+      break
+    case 'messages':
+      renderMessages(data || [])
+      break
+    case 'close':
+      break
+  }
 })
 
 const handleSubmit = (event) => {
   event.preventDefault()
-  const message = $input?.value || ''
+  const message = input?.value || ''
   if (message === '') return
 
-  socket.send(message)
-  $input.value = ''
+  const nickname = getNickname()
+  const _message = {
+    type: 'message',
+    data: { nickname, body: message },
+  }
+  socket.send(JSON.stringify(_message))
+  input.value = ''
 }
 
-$form.addEventListener('submit', handleSubmit)
-$button.addEventListener('click', handleSubmit)
+chatForm.addEventListener('submit', handleSubmit)
+button.addEventListener('click', handleSubmit)
+
+function renderMessages(messages) {
+  const chatList = document.querySelector('.chat-list')
+
+  const messageElems = messages.map((message) => {
+    const li = document.createElement('li')
+    li.className = 'message'
+    const nickname = document.createElement('div')
+    nickname.className = 'nickname'
+    nickname.textContent = message.nickname
+    const body = document.createElement('div')
+    body.className = 'body'
+    body.textContent = message.body
+    li.append(nickname, body)
+    return li
+  })
+
+  chatList.append(...messageElems)
+}
+
+function renderConnectedNumber(data = 0) {
+  const userCount = document.querySelector('.user-count')
+  userCount.textContent = `현재 접속자 수: ${data}`
+}
+
+function getNickname() {
+  return JSON.parse(localStorage.getItem('nicknames')).at(-1)
+}
