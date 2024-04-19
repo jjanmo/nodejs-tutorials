aa34f19cadbfcc9db9791ceabc3c060f9a6ef374
jjanmo <jjanmo07@hanmail.net>
2024-04-20 01:42:02 +0900

    fix: 타이핑 수정

--- project3/src/controllers/index.ts
+++ project3/src/controllers/index.ts
@@ -1,28 +1,9 @@
 import { Request, Response } from 'express'
 
-const dummyMessages = [
-  {
-    nickname: 'jjanmo',
-    body: '안녕하세요',
-  },
-  {
-    nickname: 'leonard',
-    body: '하욤!!!',
-  },
-  {
-    nickname: 'jjanmo',
-    body: '잘지내니? 오랜만이쿤!!',
-  },
-  {
-    nickname: 'leonard',
-    body: '너도 오랜만이구나!!',
-  },
-]
-
 export const home = (req: Request, res: Response) => {
   res.render('home', { title: 'Home' })
 }
 
 export const chat = (req: Request, res: Response) => {
-  res.render('chat', { title: 'Chat', messages: dummyMessages })
+  res.render('chat', { title: 'Chat' })
 }
--- project3/src/types.ts
+++ project3/src/types.ts
@@ -1,9 +1,15 @@
+import { WebSocket } from 'ws'
+
 export type ChatMessage = {
   nickname: string
   body: string
 }
-export type MessageType = 'messages' | 'connection' | 'close'
+export type MessageType = 'messages' | 'connection' | 'close' // client → server 로 보낼 때 메시지 타입
 export type Message<T> = {
   type: MessageType
   data: T
+}
+
+export type Sockets = {
+  [key: string]: WebSocket
 }
