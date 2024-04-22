ae557a44eb02d7ea5092ee640e51d9a67bddfc6e
jjanmo <jjanmo07@hanmail.net>
2024-04-23 00:16:10 +0900

    fix: 테이블 존재 유무 체크 로직 추가

--- project3/src/db.ts
+++ project3/src/db.ts
@@ -1,12 +1,19 @@
 import Database from 'better-sqlite3'
 import path from 'path'
 
-const initializeDB = () => {
+const TABLE_NAME = 'user'
+
+function initializeDB() {
   const db = new Database(path.join(__dirname, 'data.db'))
 
-  db.exec(
-    'CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, nickname VARCHAR(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)'
-  )
+  const statement = db.prepare("SELECT name FROM sqlite_master WHERE type='table' AND name=?")
+  const result = statement.get(TABLE_NAME)
+
+  if (!result) {
+    db.exec(
+      'CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, nickname VARCHAR(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)'
+    )
+  }
   return db
 }
 
