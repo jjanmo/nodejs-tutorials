966b28a883731d801801a9513bcbfb5e0f060b53
jjanmo <jjanmo07@hanmail.net>
2024-04-25 22:33:03 +0900

    fix: 데이터베이스 초기화 코드 수정

--- project3/src/db.ts
+++ project3/src/db.ts
@@ -3,20 +3,15 @@ import path from 'path'
 
 const TABLE_NAME = 'user'
 
-function initializeDB() {
-  const db = new Database(path.join(__dirname, 'data.db'))
+const db = new Database(path.join(__dirname, 'data.db'), { verbose: console.log })
 
-  const statement = db.prepare("SELECT name FROM sqlite_master WHERE type='table' AND name=?")
-  const result = statement.get(TABLE_NAME)
+const statement = db.prepare("SELECT name FROM sqlite_master WHERE type='table' AND name=?")
+const result = statement.get(TABLE_NAME)
 
-  if (!result) {
-    db.exec(
-      'CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, nickname VARCHAR(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)'
-    )
-  }
-  return db
+if (!result) {
+  db.exec(
+    'CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, nickname VARCHAR(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)'
+  )
 }
-
-const db = initializeDB()
 
 export default db
