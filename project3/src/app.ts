6fcf55aabf222eaa015bda2788c263f41127f505
jjanmo <jjanmo07@hanmail.net>
2024-04-25 22:38:17 +0900

    fix: 라우터 분리

--- project3/src/app.ts
+++ project3/src/app.ts
@@ -1,16 +1,20 @@
 import express from 'express'
 import path from 'path'
-import * as controller from './controllers'
+import * as pageController from './controllers'
+import * as userController from './controllers/user'
 
 const app = express()
 
 app.set('view engine', 'pug')
 app.set('views', process.cwd() + '/src/views')
+app.use(express.json()) // express built-in body-parser
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.get('/', controller.home)
-app.get('/chat', controller.chat)
+app.get('/', pageController.home)
+app.get('/chat', pageController.chat)
+app.post('/nickname', userController.postNickname)
+
 app.get('/*', (req, res) => res.redirect('/'))
 
 export default app
