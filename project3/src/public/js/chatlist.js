82f0a5f80e49916290eeb99e2a8c201fb60eacb0
jjanmo <jjanmo07@hanmail.net>
2024-04-27 23:45:16 +0900

    fix: 채팅리스트에서 폼 로직 추가

--- project3/src/public/js/chatlist.js
+++ project3/src/public/js/chatlist.js
@@ -1,1 +1,21 @@
-console.log('chatlist')
+const form = document.querySelector('form')
+const createBtn = document.querySelector('.create-btn')
+const roomInput = document.querySelector('#room')
+
+const handleSubmit = async (e) => {
+  e.preventDefault()
+  const roomName = roomInput.value
+  if (roomName === '') {
+    alert('채팅방을 입력해주세요.')
+    return
+  }
+
+  window.location.assign('/chatroom')
+}
+
+const init = () => {
+  form.addEventListener('submit', handleSubmit)
+  createBtn.addEventListener('click', handleSubmit)
+}
+
+init()
